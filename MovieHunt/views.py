85d899347931ab1e5e354855b9e764ddb0528b4d
aayushi-as <aayushisutaria017@gmail.com>
2020-11-18 11:00:23 +0530

    popularity_based integrated with django

--- MovieHunt/views.py
+++ MovieHunt/views.py
@@ -5,8 +5,14 @@ import joblib
 def home(request):
     popular_movies = joblib.load('popularity_model.sav')
     popular_movies = popular_movies['title'].to_list()
-    popular_movies = popular_movies[:10]
-    return render(request, 'index.html', {"popular_movies": popular_movies})
+    popular_movies = popular_movies[:6]
+
+    movie_posters = ["The_shawshank_redemption.jpg", "The_godfather.jpg", "Dilwale_Dulhania_Le_Jayenge_poster.jpg", 
+    "The_dark_knight.jpg","Fight club.jpg", "Pulp_fiction.jpg"
+    ]
+
+    return render(request, 'index.html', {"popular_movies": popular_movies, "movie_posters": movie_posters})
+
 
 def about(request):
     return render(request, "about.html")
